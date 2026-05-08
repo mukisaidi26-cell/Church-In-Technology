298837c40fe5c9782dd97d08fcfdf3a780a85c91
atreavez <alvin69david@gmail.com>
2026-05-08 15:10:48 +0300

    Worked on the deposit and withdraw

--- Assignments/cit-java-week3/src/main/java/org/example/BankAccount.java
+++ Assignments/cit-java-week3/src/main/java/org/example/BankAccount.java
@@ -1,24 +1,39 @@
 package org.example;
 
-public class BankAccount {
+public class BankAccount { //Bank Account class
         String accountNumber;
         String ownerName;
         double balance;
         boolean isActive;
 
-        void deposit(double amount) {
+        void deposit(double amount) {//Deposit method
             balance = balance + amount;
-            System.out.println("Deposited UGX " + amount + ".New Balance is " + balance);
+            System.out.println("Deposited UGX " + amount + ". New Balance is " + balance);
         }
 
-        void withdraw(double amount) {
+        void withdraw(double amount) { //Withdraw method
             balance = balance - amount;
-            System.out.println("Withdrawn UGX " + amount + "New Balance is " + balance);
+            System.out.println("Withdrawn UGX " + amount + " New Balance is " + balance);
         }
 
         void printSummary() {
             System.out.println("Account: " + accountNumber + "Owner Name: " + ownerName);
-            System.out.println("Balance: UGX " + balance + "Active: " + isActive);
+            System.out.println("Balance: UGX " + balance + " Active: " + isActive);
         }
 
+        //Creating methods
+        public static void main(String[] args) {
+            BankAccount bankAccount = new BankAccount();
+
+            bankAccount.accountNumber = "123456789";
+            bankAccount.ownerName = "Waluube Alvin David";
+            bankAccount.balance = 38000;
+            bankAccount.isActive = true;
+
+            bankAccount.deposit(8000);
+            bankAccount.withdraw(1000);
+            bankAccount.printSummary();
+        }
 }
+
+
