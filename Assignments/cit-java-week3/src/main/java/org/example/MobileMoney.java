4f08bbdb0b20af92139f0b1d7c4d41936c3a20e3
atreavez <alvin69david@gmail.com>
2026-05-08 19:07:08 +0300

    Worked on a simple mobile money

---
+++ Assignments/cit-java-week3/src/main/java/org/example/MobileMoney.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public class MobileMoney {
+    String senderName;
+    String receiverName;
+    double balance;
+    String phoneNumber;
+    int pin;
+
+    void sendMoney(String senderName, String receiverName, double amount) {
+        if (amount < balance) {
+            System.out.println("You have insufficient funds to complete this transaction. Borrow by dailing *185#");
+        } else if (amount > balance) {
+            balance = balance - amount;
+            System.out.println("You have successfully sent UGX " + amount + " to " + receiverName + ". Your new balance is UGX" + balance);
+        }
+    }
+
+    void receiveMoney(String senderName, String receiverName, double amount) {
+        balance = balance + amount;
+        System.out.println("You have received UGX " + amount + "from " + senderName + ". Your new account balance is " + balance);
+    }
+
+    public static void main (String[] args) {
+        MobileMoney account = new MobileMoney();
+
+        account.senderName = "Alvin David";
+        account.receiverName = "Tagoole David";
+        account.balance = 80000;
+        account.phoneNumber = "0758862363";
+        account.pin = 5555;
+
+        account.sendMoney("Alvin David", "Drena Jenifer", 20000);
+        account.receiveMoney("Drena Jennifer", "Alvin David", 1100);
+    }
+}
