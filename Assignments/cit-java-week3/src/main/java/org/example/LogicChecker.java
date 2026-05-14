1890be34d824a4a978ee0ac096c84f716b594899
atreavez <alvin69david@gmail.com>
2026-05-14 23:08:32 +0300

    Working on the logic checkers

--- Assignments/cit-java-week3/src/main/java/org/example/LogicChecker.java
+++ Assignments/cit-java-week3/src/main/java/org/example/LogicChecker.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class LogicChecker {
     public static void main(String[] args) {
-
+        boolean isLoggedIn = true;
+        boolean isAdmin = false;
+        System.out.println("Regular User: " + (isLoggedIn && !isAdmin));
+        System.out.println("Has access: " + (isLoggedIn || isAdmin));
+        System.out.println("Not Logged in: " + (!isLoggedIn));
     }
 }
