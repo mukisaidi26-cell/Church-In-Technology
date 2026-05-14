ba60443774f3b71c3082a9afae9df7e434024096
atreavez <alvin69david@gmail.com>
2026-05-14 23:30:58 +0300

    Worked on Java Operators

---
+++ Assignments/cit-java-week3/src/main/java/org/example/JavaOperators.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class JavaOperators {
+    public static void main(String[] args) {
+        int itemPrice = 2000;
+        int shippingCost = 4000;
+
+        int sum = itemPrice + shippingCost;
+
+        System.out.println("Sum: " + sum);
+    }
+}
